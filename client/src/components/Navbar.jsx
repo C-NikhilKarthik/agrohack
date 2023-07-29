b905a90ca1c6b5b7495983d0e9df6d4a2d6bb664
C-NikhilKarthik <nikhilkarthik241103@gmail.com>
2023-07-30 03:17:28 +0530

    frontend and backend connected

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import NavbarButtons from './Navbar/NavbarButtons'
 
 export default function Navbar() {
   return (
-    <nav className='w-full z-50 fixed top-0 left-0 p-4 bg-slate-100/30 backdrop-blur dark:bg-slate-900/70 flex justify-between'>
+    <nav className='w-full z-30 fixed top-0 left-0 p-4 bg-slate-100/30 backdrop-blur dark:bg-slate-900/70 flex justify-between'>
         <div className='text-purple-600 dark:text-purple-300 font-semibold text-xl'>AgriSenseAI</div>
         <div className='gap-4 flex text-black items-center dark:text-slate-200'>
             <NavbarButtons text={"About"}/>
